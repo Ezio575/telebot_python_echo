af72a5a9f8c673c25cc35fd411ac4af671b6d388
Ezio575 <61089579+Ezio575@users.noreply.github.com>
2020-04-13 00:52:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import telebot
+
+bot = telebot.TeleBot('1198216242:AAFaAwFAOOm9FbBFJqOumMBuzeS5Pmtg-_s') # создаем переменную + пишем полученный токен
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start') #Отправка ответа от бота
+
+@bot.message_handler(content_types=['text']) #учим бота отвечать на простые запросы
+def send_text(message):
+    if message.text == 'Привет':
+            bot.send_message(message.chat.id, 'Привет, друг')
+    elif message.text == 'Как тебя зовут':
+            bot.send_message(message.chat.id, 'Меня зовут Роксанна, я бот')
+    elif message.text == 'Пока':
+            bot.send_message(message.chat.id, 'До связи')
+
+bot.polling() #Запускает бота, бот проверяет сервер на наличие сообщений
